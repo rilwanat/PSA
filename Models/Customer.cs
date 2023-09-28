89d2a8646e42006a9e9f5e87b3a916717a030476
Rilwan <rilwan.at@gmail.com>
2023-09-28 10:52:36 +0100

    created Models
    - Merchant and Customer classes to represent the data models

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace PSA.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string NationalIdNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CustomerNumber { get; set; }
+        public string TransactionHistory { get; set; }
+    }
+}
---
+++ Models/Merchant.cs
@@ -0,0 +1,14 @@
+﻿namespace PSA.Models
+{
+    public class Merchant
+    {
+        public int Id { get; set; }
+        public string BusinessIdNumber { get; set; }
+        public string BusinessName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactSurname { get; set; }
+        public DateTime DateOfEstablishment { get; set; }
+        public string MerchantNumber { get; set; }
+        public decimal? AverageTransactionVolume { get; set; }
+    }
+}

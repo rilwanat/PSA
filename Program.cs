9acd405b95d2583a779df0934d52174251e42444
Rilwan <rilwan.at@gmail.com>
2023-09-28 10:39:34 +0100

    NuGet packages
    - added NuGet packages for Entity Framework Core and PostgreSQL
    - configured connection string

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Configuration
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

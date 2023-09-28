de3a1cb93e5a54087bb7e85995fcf7b0f6960cba
Rilwan <rilwan.at@gmail.com>
2023-09-28 10:46:00 +0100

    modified Connection string

--- Program.cs
+++ Program.cs
@@ -3,10 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-// Configuration
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
